cf7ee1a69dcd29a72ba7f74dc09aeed8857e64b4
patrick mugambi <patrick@kali.kali>
2022-09-29 15:18:09 +0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './views/HomePage';
+import React from "react";
+import "./index.css";
+
+import Navbar from "./components/Navbar";
+import Login from "./views/loginPage";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./utils/PrivateRoute";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/ProtectedPage";
+import HomePage from "./views/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-   </div>
+ <>
+    <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route index element={<HomePage />} />
+
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+    
+      </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+
+ </>
   );
 }
 
