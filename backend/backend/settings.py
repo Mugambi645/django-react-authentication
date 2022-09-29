5f76d33c3072fe028f2f3d94eda90f226050d334
patrick mugambi <patrick@kali.kali>
2022-09-29 13:52:24 +0300

    create app accounts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'accounts'
 ]
 
 MIDDLEWARE = [
